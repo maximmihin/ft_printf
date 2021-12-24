b4acf6756db747e07ce4426073dc33163efcfb61
Georgette Radagast <gradagas@ev-d2.kzn.21-school.ru>
2021-12-24 07:28:54 +0300

    hot-fix

--- ft_hex.c
+++ ft_hex.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-include "ft_printf.h"
+#include "ft_printf.h"
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
